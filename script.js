aab655b1cc1201b1215e0d92fb27d9b5d7cf79f7
LumiNovryM <luminovrymekel@gmail.com>
2022-09-11 11:59:46 +0700

    membuat file index.html & script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Selamat Datang di Old Henry Bot");
+
+let angka = prompt("Masukkan Angka!");
+let lagi = true;
+
+
+if(angka % 2 == 0){
+    alert(angka + " adalah bilangan GENAP")
+}else if(angka % 2 == 1){
+    alert(angka + " adalah bilangan GANJIL")
+}else{
+    alert("Disuruh Masukkan Angka Ya kak!");
+    alert("Bukan Masukkin Huruf")
+}
+
+alert("Terima Kasih")
